78026d740627219e4fd09b5c4713f4b46de0b7dc
imad <imad.live1990@gmail.com>
2020-05-03 04:12:03 +0200

    create entities and relation between entity

---
+++ src/main/java/org/sid/cinema/entities/Categorie.java
@@ -0,0 +1,36 @@
+package org.sid.cinema.entities;
+
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Categorie {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	//la taille du caractere dans la table BD par default 255
+	@Column(length=75)
+	private String name;
+		
+	@OneToMany(mappedBy = "categorie")
+	private Collection<Film> films;
+
+}
