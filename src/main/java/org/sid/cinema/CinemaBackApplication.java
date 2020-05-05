563a6535ed2cf6c4a3fcb4a60e7055381a49a3e9
imad <imad.live1990@gmail.com>
2020-05-06 01:03:24 +0200

    add restConfiguration for retourne id

--- src/main/java/org/sid/cinema/CinemaBackApplication.java
+++ src/main/java/org/sid/cinema/CinemaBackApplication.java
@@ -1,16 +1,20 @@
 package org.sid.cinema;
 
+import org.sid.cinema.entities.Film;
 import org.sid.cinema.service.ICinemaInitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
 public class CinemaBackApplication implements CommandLineRunner{
 
 	@Autowired
 	private ICinemaInitService cinemaInitService;
+	@Autowired
+	private RepositoryRestConfiguration restConfiguration;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CinemaBackApplication.class, args);
@@ -18,6 +22,8 @@ public class CinemaBackApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
+		//inclure le id dans le resultat du recherche
+		restConfiguration.exposeIdsFor(Film.class);
 			cinemaInitService.initVilles();
 			cinemaInitService.initCinemas();
 			cinemaInitService.initSalles();
